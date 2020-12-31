d4fe68ea437826dcc976c8e1423d028e84515f9a
gxd <gxd523@gmail.com>
2020-12-31 12:16:07 +0800

    no message

--- app/src/main/java/com/demo/mvvm/ui/material/ScrollingActivity.kt
+++ app/src/main/java/com/demo/mvvm/ui/material/ScrollingActivity.kt
@@ -3,17 +3,17 @@ package com.demo.mvvm.ui.material
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.demo.mvvm.R
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_scrolling.*
 
 class ScrollingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        setSupportActionBar(tool_bar)
+        toolbar_layout.title = title
+
+        floating_action_btn.setOnClickListener { view ->
             Snackbar
                 .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
